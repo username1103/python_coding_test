6d46b9d6f7050c799b04e87b521b40f6e61c4c66
username1103 <username1103@naver.com>
2021-01-28 21:02:52 +0900

    1이 될 때까지 greedy

---
+++ greedy/1이될때까지.py
@@ -0,0 +1,19 @@
+# 25 5
+
+n, k = list(map(int, input().split()))
+
+cnt = 0
+
+while True:
+    temp = (n // k) * k
+    cnt += (n - temp)
+    n = temp
+
+    if n < k:
+        break
+
+    cnt += 1
+    n //= k
+
+cnt += (n - 1)
+print(cnt)
