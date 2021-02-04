20ea87d5ba4e4b6e9a20bbc748dc708496b57383
username1103 <username1103@naver.com>
2021-02-04 16:22:01 +0900

    greedy

--- greedy/모험가길드.py
+++ greedy/모험가길드.py
@@ -6,7 +6,7 @@ data = sorted(list(map(int, input().split())))
 
 result = 0
 
-for i in range(max(data)):
+for i in range(1, max(data) + 1):
     cnt = data.count(i)
     if cnt != 0:
         result += (cnt // i)
