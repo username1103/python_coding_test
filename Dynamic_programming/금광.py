57d888b445d938efe53519c86dc5c056dc8af718
username1103 <username1103@naver.com>
2021-02-14 12:31:46 +0900

    Dynamic_programming 문제풀이 복습

--- Dynamic_programming/금광.py
+++ Dynamic_programming/금광.py
@@ -5,14 +5,20 @@
 # 1 3 1 5 2 2 4 1 5 0 2 3 0 6 1 2
 
 
-t = int(input())
+t = int(input())  # 횟수 입력받기
 
 for _ in range(t):
-    n, m = map(int, input().split())
-    cave = list(map(int, input().split()))
-    d = [0] * (m * n)
+    n, m = map(int, input().split())  # 금광의 크기 입력받기
+    cave = list(map(int, input().split()))  # 금광 입력받기
+
+    d = [0] * (m * n)  # dp테이블: 해당위치까지 캐서 올수 있는 최대 금의 양
+
+    # dp테이블 첫 열 초기화
     for i in range(n):
         d[i * m] = cave[i * m]
+
+    # 1열 부터 시작해서 왼쪽, 왼쪽위, 왼쪽아래에 값들이 존재한다면 그 값들중 최대값을 찾아 현재 값과 더하여
+    # 값을 dp테이블에 저장
     for j in range(1, m):
         for i in range(n):
             temp = []
@@ -23,6 +29,7 @@ for _ in range(t):
                 temp.append(d[(i + 1) * m + (j - 1)])
             d[i * m + j] = max(temp) + cave[i * m + j]
 
+    # 마지막열중 가장 큰값 으로 result 갱신
     result = 0
     for i in range(n):
         result = max(result, d[i * m + (m - 1)])
--- Dynamic_programming/정수삼각형.py
+++ Dynamic_programming/정수삼각형.py
@@ -1,23 +1,49 @@
-n = int(input())
+# BaekJoon 1932
+#         7
+#       3   8
+#     8   1   0
+#   2   7   4   4
+# 4   5   2   6   5
+# 위 그림은 크기가 5인 정수 삼각형의 한 모습이다.
+
+# 맨 위층 7부터 시작해서 아래에 있는 수 중 하나를 선택하여 아래층으로 내려올 때,
+# 이제까지 선택된 수의 합이 최대가 되는 경로를 구하는 프로그램을 작성하라.
+# 아래층에 있는 수는 현재 층에서 선택된 수의 대각선 왼쪽 또는 대각선 오른쪽에 있는 것 중에서만 선택할 수 있다.
+
+# 삼각형의 크기는 1 이상 500 이하이다. 삼각형을 이루고 있는 각 수는 모두 정수이며, 범위는 0 이상 9999 이하이다.
+
+# 입력
+# 첫째 줄에 삼각형의 크기 n(1 ≤ n ≤ 500)이 주어지고, 둘째 줄부터 n+1번째 줄까지 정수 삼각형이 주어진다.
+
+# 출력
+# 첫째 줄에 합이 최대가 되는 경로에 있는 수의 합을 출력한다.
+
+
+n = int(input())  # 삼각형의 크기
+
+# 삼각형 입력 받기
 data = []
 for _ in range(n):
     data.append(list(map(int, input().split())))
 
+# dp 테이블 : 해당 값까지 왔을 때 최댓 값
 d = [[0] * i for i in range(1, n + 1)]
-d[0][0] = data[0][0]
+d[0][0] = data[0][0]  # dp 테이블 초기화
 
 result = 0
+
+# 해당 값이 처음 값인지 끝 값인지 중간 값인지 확인하여 최댓값을 설정
 for i in range(1, len(data)):
     for j in range(len(data[i])):
         if j == 0:
-            d[i][j] = max(d[i][j], d[i-1][j] + data[i][j])
+            d[i][j] = d[i-1][j] + data[i][j]
         elif j == len(data[i]) - 1:
-            d[i][j] = max(d[i][j], d[i-1][j-1] + data[i][j])
+            d[i][j] = d[i-1][j-1] + data[i][j]
         else:
-            d[i][j] = max(d[i][j], d[i-1][j] + data[i]
-                          [j], d[i-1][j-1] + data[i][j])
+            d[i][j] = max(d[i-1][j] + data[i][j], d[i-1][j-1] + data[i][j])
 
+        # 마지막줄에 도달하면 result값을 최댓값으로 갱신
         if i == len(data) - 1:
             result = max(result, d[i][j])
 
-print(result)
+print(max(d[n-1]))
