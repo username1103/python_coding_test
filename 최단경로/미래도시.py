bd68d2c12ea93ea8fb8086e5cb1a2446e8f84197
username1103 <username1103@naver.com>
2021-02-17 16:22:53 +0900

    최단경로 복습

--- 최단경로/미래도시.py
+++ 최단경로/미래도시.py
@@ -17,27 +17,32 @@ import sys
 input = sys.stdin.readline
 INF = int(1e9)
 
-n, m = map(int, input().split())
+n, m = map(int, input().split())  # 도시의 수와 간선의 수
 
-graph = [[INF] for _ in range(n + 1)]
+graph = [[INF] for _ in range(n + 1)]  # 플루이드 와샬 알고리즘을 위한 테이블
 
+# 테이블 초기화
 for _ in range(m):
     a, b = map(int, input().split())
     graph[a][b] = 1
     graph[b][a] = 1
 
+# 경유지와 목적지
 x, k = map(int, input().split())
 
+# 자기 자신을 0으로 테이블 초기화
 for i in range(1, n + 1):
     for j in range(1, n + 1):
         if i == j:
             graph[i][j] = 0
 
+# i에서 j로 갈 때, 현재 구해진 값과 k를 경유해서 갈 경우의 값을 비교하여 작은 값으로 갱신
 for k in range(1, n + 1):
     for i in range(1, n + 1):
         for j in range(1, n + 1):
             graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
 
+# 최종 거리
 distance = graph[1][k] + graph[k][x]
 
 if distance >= INF:
--- 최단경로/전보.py
+++ 최단경로/전보.py
@@ -8,15 +8,17 @@ import heapq
 input = sys.stdin.readline
 INF = int(1e9)
 
-n, m, start = map(int, input().split())
-graph = [[] for i in range(n + 1)]
-distance = [INF] * (n + 1)
+n, m, start = map(int, input().split())  # 도시 수, 통로 수, 시작 도시
+graph = [[] for i in range(n + 1)]  # 인접 리스트를 통한 간선 관리
+distance = [INF] * (n + 1)  # 다익스트라를 위한 테이블 초기화
 
+# 각각의 간선들의 정보를 인접리스트에 저장
 for _ in range(m):
     x, y, z = map(int, input().split())
     graph[x].append((y, z))
 
 
+# 다익스트라 알고리즘
 def dijkstra(start):
     q = []
     heapq.heappush(q, (0, start))
@@ -35,8 +37,8 @@ def dijkstra(start):
 
 dijkstra(start)
 
-count = 0
-max_distance = 0
+count = 0  # 연결된 도시의 수
+max_distance = 0  # 가장 멀리 있는 도시와 거리
 for d in distance:
     if d != INF:
         count += 1
