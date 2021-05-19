38159cde52432b5bffd9d4bacce8453aef52a7eb
username1103 <username1103@naver.com>
2021-05-19 14:28:46 +0900

    가사검색 내 답안 수정 - 55점.. 효율성 떨어짐 => 이진탐색 사용해야 함

--- search/가사검색.py
+++ search/가사검색.py
@@ -4,60 +4,28 @@ words = ["frodo", "front", "frost", "frozen", "frame", "kakao"]
 queries = ["fro??", "????o", "fr???", "fro???", "pro?"]
 
 
-def findIndex(query, direction):
-    if direction == 'r':
-        result = len(query)
-    else:
-        result = 0
+def solution(words, queries):
+    result = []
+    for i in range(len(queries)):
+        query = queries[i]
+        index = query.find("?")
+        isReverse = False
+        if index == 0:
+            isReverse = True
+            query = query[::-1]
+            index = query.find("?")
+        count = 0
+        for word in words:
+            if isReverse:
+                if len(word) == len(query) and (word[::-1])[:index] == query[:index]:
+                    count += 1
+            else:
+                if len(word) == len(query) and word[:index] == query[:index]:
+                    count += 1
 
-    start = 0
-    end = len(query)
-    while start <= end:
-        mid = (start + end) // 2
-        if mid == len(query):
-            result = len(query)
-            break
-        if query[mid] == '?':
-            if mid >= 0 and direction == 'r' and query[mid - 1] != '?':
-                result = mid
-                break
-            elif mid < (len(query)-1) and direction == 'l' and query[mid + 1] != '?':
-                result = mid
-                break
-            if direction == 'r':
-                end = mid - 1
-            else:
-                start = mid + 1
-        else:
-            if direction == 'r':
-                start = mid + 1
-            else:
-                end = mid - 1
+        result.append(count)
 
     return result
-
-
-def solution(words, queries):
-    answer = []
-    words.sort(key=lambda x: (len(x), x))
-    for query in queries:
-        direction = 'r'
-        if query[0] == '?':
-            direction = 'l'
-        index = findIndex(query, direction)
-        u = query[:index] if direction == 'r' else query[index + 1:]
-        count = 0
-        if direction == 'r':
-            for word in words:
-                if word.startswith(u) and len(word) == len(query):
-                    count += 1
-        elif direction == 'l':
-            for word in words:
-                if word.endswith(u) and len(word) == len(query):
-                    count += 1
-        answer.append(count)
-
-    return answer
 
 
 print(solution(words, queries))
