0db6c19806f05531014c936e786dbea4ff28f72c
username1103 <username1103@naver.com>
2021-05-24 11:24:51 +0900

    다익스트라 알고리즘 공부

--- 최단경로/good_dijkstra.py
+++ 최단경로/good_dijkstra.py
@@ -23,6 +23,7 @@ start = int(input())
 graph = [[] for i in range(n + 1)]
 distance = [INF] * (n + 1)
 
+# a번 노드에서 c번 노드로 가는데 비용이 b이다.
 for _ in range(m):
     a, b, c = map(int, input().split())
     graph[a].append((b, c))
@@ -34,11 +35,15 @@ def dijkstra(start):
     distance[start] = 0
     while q:
         dist, now = heapq.heappop(q)
+
+        # 방문했던 노드라면 건너 뜀
         if distance[now] < dist:
             continue
 
+        # 해당 노드와 연결된 노드들을 확인
         for i in graph[now]:
             cost = dist + i[1]
+            # 현재 노드를 거쳐 연결된 노드로 가는 경로가 더 짧은경우
             if cost < distance[i[0]]:
                 distance[i[0]] = cost
                 heapq.heappush(q, (cost, i[0]))
@@ -47,6 +52,7 @@ def dijkstra(start):
 dijkstra(start)
 
 for i in range(1, n + 1):
+    # 도달할 수 없는 경우 값이 계속 INF로 남아있음.
     if distance[i] == INF:
         print("INFINITY")
     else:
