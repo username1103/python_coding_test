c8f1f4402e2cb081d4ac964ae0fc7facbb78624a
myeongil <username1103@naver.com>
2022-05-16 09:24:34 +0000

    slice_two_dimensional_array 예제 추가

--- module/slice_two_dimensional_array.py
+++ module/slice_two_dimensional_array.py
@@ -1,2 +1,14 @@
 def slice_two_dimensional_array(arr,rowStart,rowEnd,colStart,colEnd):
     return [row[colStart:colEnd] for row in arr[rowStart:rowEnd]]
+
+
+if __name__ == "__main__":
+    matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]]
+    print("Before Matrix")
+    for i in range(len(matrix)):
+        print(matrix[i])
+    matrix = slice_two_dimensional_array(matrix, 0,2,0,2)
+    print()
+    print("After Matrix")
+    for i in range(len(matrix)):
+        print(matrix[i])
